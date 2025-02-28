8aaab6b45fc8cc1d5a9ba61cb7203aa38cb74fa9
Nataliia Musiienko <basan.1994.15@gmai.com>
2025-02-28 01:02:33 +0100

    Andere Structure

--- pages/trends_and_dependency_analysis.py
+++ pages/trends_and_dependency_analysis.py
@@ -2,11 +2,13 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import ast
+
 
 #Bekommen Daten von csv-Datai, die Information über Rating vom Filmen enthalten
-df_rating=pd.read_csv(r"C:\Users\49160\Project-Bilal\archive\ratings.csv")
+df_rating=pd.read_csv(r"ratings.csv")
 #Bekommen Daten mit Filmenmetadaten von csv-Datai(Laufzeit,Budget...).
-df_metadata=pd.read_csv(r"C:\Users\49160\Project-Bilal\archive\movies_metadata.csv")
+df_metadata=pd.read_csv(r"movies_metadata.csv")
 #Löschen alle Zeile, wo im bestimmte Spallte  NaN order nicht numeric Daten geben
 
 def clean_not_numbers_value(df,subset):
@@ -43,7 +45,7 @@ def prepare_data_for_rating_analysys(df_metadata,df_rating,x_value):
     print(rating_y.size)
     print(values_x.size)
     return values_x, rating_y
-values_x,rating_y=prepare_data_for_rating_analysys('budget')
+#values_x,rating_y=prepare_data_for_rating_analysys(df_metadata=df_metadata,df_rating=df_rating,'budget')
 
 #Zeigen Zusammenhang zwischen Bewertunen(rating_y Argument) und Values for X-Axis(values_x Argument)
 #x_axis wird X-Axis Label
@@ -85,8 +87,70 @@ def show_rating_correlation(x_axis,values_x, rating_y):
     plt.legend()
     #diagramm anzeigen
     st.pyplot(plt)
-st.write("Task 7")
+
+st.subheader("Analyse des Zusammenhangs zwischen Budget und Bewertung order Laufzeit von Filmen")
+    
+
 x_parameter=st.selectbox("Bitte wählen Sie,das zweite Kriterium, das Sie für die Analyse verwenden möchten:", ["budget", "runtime"])
 
-values_x,rating_y=prepare_data_for_rating_analysys(x_parameter)
+values_x,rating_y=prepare_data_for_rating_analysys(df_metadata, df_rating,x_parameter)
 show_rating_correlation(x_parameter,values_x,rating_y)
+st.markdown("<br><br>", unsafe_allow_html=True)   
+st.subheader("Analyse der durchschnittlichen Filmbewertung pro Genre")
+
+
+#Lösche doppelt Wete in 'id' Spalte
+df_metadata = df_metadata.drop_duplicates(subset='id', keep='first')
+#Konvertiert zu String
+df_metadata['id'] = df_metadata['id'].astype(str)
+df_rating['movieId'] = df_rating['movieId'].astype(str)
+df_metadata=df_metadata[['id','genres']].dropna()
+df_rating=df_rating[['movieId','rating']].dropna()
+
+
+#Konvertieren eine String zu eine Liste,wenn möglich ist
+def convert_string_to_list(value):
+    if isinstance(value, str):  
+        try:
+            return ast.literal_eval(value)
+        except (ValueError, SyntaxError):
+            return []  # Falls Fehler auftreten, gib eine leere Liste zurück
+    return value 
+# 2 DataFrames zusammenfügen
+df_merged=pd.merge(df_metadata,df_rating,left_on='id',right_on='movieId',how='inner')
+df_merged = df_merged.sample(n=2000)  
+#Values in 'genre' Spalte in eine String konvertieren
+df_merged['genres'] = df_merged['genres'].apply(lambda x:convert_string_to_list(x)) 
+#'genre_names' Spalte erstellen
+#Namen von Genres von 'genres' in genre_names' übertragen
+df_merged['genre_names'] = df_merged['genres'].apply(lambda x: [genre['name'] for genre in x])
+
+df_merged['genre_names'] = df_merged['genre_names'].apply(lambda x:convert_string_to_list(x)) 
+#Für jerer element in Liste in  'genre_nammes' Spalte sepatrate Zeile erstellen
+df_merged['genre_names']=df_merged['genre_names'].apply(lambda li:[i for i in li])
+#Füllen entstehende Zeile in andere Zeilen mit entsprechende Values
+exploded_df = df_merged.explode('genre_names')
+
+#Finden Mittlewert von Bewertungen pro ein Film
+rating_per_movie=exploded_df.groupby('movieId')['rating'].mean()
+#Ene Serie und ein DataFrame zusammenfügen
+df_rating_per_movie=pd.merge(exploded_df[['movieId','genre_names']],rating_per_movie,on='movieId',how='inner')
+#eine Serie mit Mittlewert von Bewertungen pro ein Genre erstellen
+rating_per_genre=df_rating_per_movie.groupby('genre_names')['rating'].mean()
+
+
+
+
+# Balkendiagramm erstellen
+plt.figure(figsize=(15, 8))
+plt.bar(rating_per_genre.index, rating_per_genre.values, color="royalblue")
+
+# Achsenbeschriftungen und Titel setzen
+plt.xticks(rotation=45)
+plt.xlabel("Genre")
+plt.ylabel('Durchschnittliche Bewertung')
+plt.title("Durchschnittliche Bewertung pro Genre")
+
+# Diagramm anzeigen
+st.pyplot(plt)
+
